07d8669bf716c69e00d5bba9eb03e04c85cdf31b
dragontux <lemrdragons@gmail.com>
2014-09-10 19:37:44 -0400

    Update wordDictionary.java
    Wrong class hehe

--- wordDictionary.java
+++ wordDictionary.java
@@ -2,17 +2,32 @@ import java.util.Scanner;
 import java.util.LinkedList;
 import java.io.*;
 
-public class guessingGame
+public class wordDictionary
 {
-        wordDictionary words;
+	private String[] words;
 
-        public guessingGame( )
-        {
+	public wordDictionary( )
+	{
 
-        }
+	}
 
-        public static void main( String[] args )
-        {
-                System.out.println( "I'm thinking of a word, can you guess what it is?" );
-        }
+	public wordDictionary( String file_name )
+	{
+		System.out.println( "Opening " + file_name + " as the dictionary." );
+	}
+
+	public boolean readDictionary( String file_name ){
+		Scanner reader;
+		boolean ret = false;
+
+		try {
+			reader = new Scanner( new FileReader( file_name ));
+			ret = true;
+
+		} catch ( IOException e ){
+			System.out.println( "Could not open file " + file_name );
+		}
+
+		return ret;
+	}
 }
