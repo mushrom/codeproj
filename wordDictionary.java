5f019a672ab91f45dca05df35557d9b58cccd93e
dragontux <lemrdragons@gmail.com>
2014-09-10 19:19:17 -0400

    Create wordDictionary.java

---
+++ wordDictionary.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class wordDictionary
+{
+        public wordDictionary( )
+        {
+        }
+        
+        public wordDictionary( String file_name )
+        {
+                System.out.println( "Opening " + file_name + " as the dictionary." );
+        }
+}
