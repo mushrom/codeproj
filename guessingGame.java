9cf2e5a78715cee2d5fdba29300ae9f1d13562f4
dragontux <lemrdragons@gmail.com>
2014-09-10 19:18:06 -0400

    Adding initial main file

---
+++ guessingGame.java
@@ -0,0 +1,11 @@
+public class guessingGame
+{
+        wordDictionary words;
+        public guessingGame( )
+        {
+        }
+        public static void main( String[] args )
+        {
+                System.out.println( "I'm thinking of a word, can you guess what it is?" );
+        }
+}
